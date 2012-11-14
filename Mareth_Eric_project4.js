c435178a5c05975c6b665bf323b573884c6852b2
EricMareth <EricMareth@fullsail.edu>
2012-11-13 20:39:14 -0500

    Second one down! Got to get the other four...

--- Mareth_Eric_project4.js
+++ Mareth_Eric_project4.js
@@ -5,9 +5,20 @@
 
 var myLibrary = function() {
 
+// Checks for a 123-456-7890 pattern.
+	var phoneCheck = function(number){
+		var numCheck = /\d{3}\-\d{3}\-\d{4}/;		//RegExp checks given string for 3 numbers, a dash, three numbers, a dash, and 4 numbers.
+		if (numCheck.test(number) === true){
+			return true;
+		}else{
+			return false;
+		};
+	};
+
+
 // URL identifier
 	var urlCheck = function(address){
-		var urlPrefix = /http:|https:/;				//regExp checks given string for "http:" or "https:" substring
+		var urlPrefix = /http:|https:/;				//RegExp checks given string for "http:" or "https:" substring
 		if (urlPrefix.test(address) === true){
 			return true;
 		}else{
@@ -15,14 +26,14 @@ var myLibrary = function() {
 		};
 	};
 	
-	
 	return{
-		"urlCheck"	:	urlCheck
-	}
-	
-	
+		"phoneCheck"	:	phoneCheck,
+		"urlCheck"		:	urlCheck
+		
+	};
 };
 
 var newLibrary = new myLibrary();
 
+console.log(newLibrary.phoneCheck("407-922-6687"));
 console.log(newLibrary.urlCheck("https://www.marethphotography.com"));
