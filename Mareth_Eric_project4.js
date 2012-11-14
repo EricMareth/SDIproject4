4700dd552faa902405a6de01f14b6230384630b6
EricMareth <EricMareth@fullsail.edu>
2012-11-13 21:34:28 -0500

    Okay, doing good with three. Need to code something other than boolean return problems.

--- Mareth_Eric_project4.js
+++ Mareth_Eric_project4.js
@@ -14,9 +14,26 @@ var myLibrary = function() {
 			return false;
 		};
 	};
+	
+	
+//Checks if string is the same pattern as an e-mail address.
+	var isEmail = function(mailAddress){
+		var mailTest = /\w*\@\w*\.\w{2,6}/,			//This RegExp will check for any number of letters before the @ symbol, any number after, and up to 6 characters after the "." to allow for suffixes like .museum and .travel.
+			atSymbol = /@/;							//RegExp checks first for an @ symbol.
+		if (mailAddress.search(atSymbol) >= 0){
 
+			if (mailTest.test(mailAddress) === true){
+				return true;
+			}else{
+				return false;
+			};
 
-// URL identifier
+		}else{
+			return false;
+		};
+	};
+
+// URL identifier.
 	var urlCheck = function(address){
 		var urlPrefix = /http:|https:/;				//RegExp checks given string for "http:" or "https:" substring
 		if (urlPrefix.test(address) === true){
@@ -28,6 +45,7 @@ var myLibrary = function() {
 	
 	return{
 		"phoneCheck"	:	phoneCheck,
+		"isEmail"		:	isEmail,
 		"urlCheck"		:	urlCheck
 		
 	};
@@ -36,4 +54,6 @@ var myLibrary = function() {
 var newLibrary = new myLibrary();
 
 console.log(newLibrary.phoneCheck("407-922-6687"));
-console.log(newLibrary.urlCheck("https://www.marethphotography.com"));
+console.log(newLibrary.isEmail("ericmareth@marethphotography.com"));
+console.log(newLibrary.urlCheck("http://www.marethphotography.com"));
+
